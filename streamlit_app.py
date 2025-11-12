6ee9308a77379313fa5ce51ea12d29e106e11518
junhyeong22 <148923070+junhyeong22@users.noreply.github.com>
2025-11-12 22:22:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -504,7 +504,7 @@ if Career:
                     return_source_documents=True,
                     chain_type_kwargs={"prompt": prompt}
                 )
-                last_error = None
+                
                 for _ in range(3):  # 최대 3번만 시도
                     try:
                         result = qa_chain.invoke({"query": query})
@@ -1014,6 +1014,7 @@ if Dreamer:
             answer_dream = st.session_state.dream_chat._call(history_dream)
             st.session_state.dream_history.append({"role": "assistant", "content": answer_dream})
         st.rerun()
+
 
 
 
