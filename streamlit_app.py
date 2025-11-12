07282cb862348fc23f842be168ce258822d747ad
junhyeong22 <148923070+junhyeong22@users.noreply.github.com>
2025-11-12 22:59:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -504,8 +504,6 @@ if Career:
                     return_source_documents=True,
                     chain_type_kwargs={"prompt": prompt}
                 )
-                last_error = None
-                success = False
                 for _ in range(3):  # 최대 3번만 시도
                     try:
                         result = qa_chain.invoke({"query": query})
@@ -514,15 +512,12 @@ if Career:
                         text = text_to_json(text)
                         st.session_state.gpt_result = text["전체 출력 결과"]
                         st.session_state.company_name_by_gpt = text["기업명"]
-                        st.session_state.source_docs = result["source_documents"]
-                        success = True
                         break
-                    except Exception as e:
-                        last_error = e
+                    except:
                         continue
                 if not success:
                     st.error(f"응답 처리에 실패했습니다: {last_error}")
-                #st.session_state.source_docs = result["source_documents"]
+                st.session_state.source_docs = result["source_documents"]
     
                 # 다시 비우기 전 최종 저장
                 st.session_state["saved_query"] = query
@@ -1018,6 +1013,7 @@ if Dreamer:
             answer_dream = st.session_state.dream_chat._call(history_dream)
             st.session_state.dream_history.append({"role": "assistant", "content": answer_dream})
         st.rerun()
+
 
 
 
